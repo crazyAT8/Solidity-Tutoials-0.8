2d9a2fa74737e8b4599c0a0d108c0e628844ff3f
crazyAT8 <88wwatson@gmail.com>
2023-08-29 22:32:20 -0500

    8/29/23

---
+++ Enum_25.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.3;
+
+contract Enum {
+    enum Status {
+        None, 
+        Pending,
+        Shipped,
+        Completed,
+        Rejected,
+        Canceled
+    }
+
+    Status public status;
+
+    struct Order {
+        address buyer;
+        Status status;
+    }
+
+    Order[] public order;
+
+    function get() view external returns (Status) {
+        return status;
+    }
+
+    function set(Status _status) external {
+        status = _status;
+    }
+
+    function ship() external {
+        status = Status.Shipped;
+    }
+
+    function seset() external {
+        delete status;
+    }
+}
+
